12f12874ac65fe8daa83797e998843e67f6427e2
Claudia Pereira Oliveira <cpo@blue10.com>
2023-10-26 12:24:01 +0200

    update

--- prisma/seed.js
+++ prisma/seed.js
@@ -48,7 +48,7 @@ async function seedProducts() {
         title: "Cuban Mug",
         description:
           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        url: "https://picsum.photos/id/24/800/800",
+        url: "https://picsum.photos/id/30/800/800",
         price: 1299,
       },
     });
@@ -128,7 +128,7 @@ async function seedProducts() {
         title: "Cup of Tea",
         description:
           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        url: "https://picsum.photos/id/60/800/800",
+        url: "https://picsum.photos/id/63/800/800",
         price: 125,
       },
     });
--- server/api/prisma/get-address-by-user/[id].js
+++ server/api/prisma/get-address-by-user/[id].js
@@ -2,8 +2,8 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
-  let product = await prisma.products.findFirst({
-    where: { id: Number(event.context.params.id) },
+  const res = await prisma.addresses.findFirst({
+    where: { userId: event.context.params.id },
   });
-  return product;
+  return res;
 });
